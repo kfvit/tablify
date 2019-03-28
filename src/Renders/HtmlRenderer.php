cc3600bc2663c015d2052d7ffe1ffbcf9f934d39
TheWorstCode <khalonen88@gmail.com>
2019-03-28 11:34:11 +0100

    Added possibility of setting a PDF footer that repeats on pages

--- src/Renders/HtmlRenderer.php
+++ src/Renders/HtmlRenderer.php
@@ -2,87 +2,97 @@
 namespace Dialect\Tablify\Renders;
 
 class HtmlRenderer implements Renderer{
-	protected $data;
-	public function __construct($data) {
-		$this->data = $data;
-	}
+    protected $data;
+    public function __construct($data) {
+        $this->data = $data;
+    }
 
-	public function render() {
-		$class = config('tablify.html')['class'];
-		$html = '<table class="'.$class.'">';
-		$html .= $this->renderHeader();
-		$html .= $this->renderBody();
-		$html .= '</table>';
+    public function render($footer = null) {
+        $class = config('tablify.html')['class'];
+        $html = '';
+        if($footer) {
+            $html .= $this->getFooterStyle();
+            $html .= '<footer>'.$footer.'</footer>';
+        }
+        $html .= '<table class="'.$class.'">';
+        $html .= $this->renderHeader();
+        $html .= $this->renderBody();
+        $html .= '</table>';
 
-		return $html;
-	}
+        return $html;
+    }
 
-	protected function renderHeader(){
-		$html = '<thead><tr>';
-		foreach($this->data['headers'] as $header){
-			$html .= '<th '.$this->getClass($header).$this->getStyle($header).$this->getId($header).'>'.$header->getValue().'</th>';
+    public function getFooterStyle(){
+        $html = '<style> footer { position: fixed; bottom: -60px; left: 0px; right: 0px; text-align: center; height: 50px;} </style>';
+        return $html;
+    }
 
-		}
-		$html .= '</tr></thead>';
-		return $html;
-	}
+    protected function renderHeader(){
+        $html = '<thead><tr>';
+        foreach($this->data['headers'] as $header){
+            $html .= '<th '.$this->getClass($header).$this->getStyle($header).$this->getId($header).'>'.$header->getValue().'</th>';
 
-	protected function renderBody(){
-		$html = '<tbody>';
-		foreach($this->data['rows'] as $row){
-			$html .= '<tr>';
-			foreach($this->data['headers'] as $header){
-				$found = false;
-				foreach($row as $key => $col){
-					if($header->getValue() == $key){
-						$html .= '<td '.$this->getClass($col).$this->getStyle($col).$this->getId($col).'>'.$col->getValue().'</td>';
-						$found = true;
-						break;
-					}
-				}
-				if(!$found){
-					$html .= '<td></td>';
-				}
-			}
-			$html .= '</tr>';
-		}
-		if(count($this->data['sums'])){
-			$html .= '<tr>';
-			foreach($this->data['headers'] as $header){
-				$found = false;
-				foreach($this->data['sums'] as $key => $col){
-					if($header->getValue() == $key){
-						$html .= '<td '.$this->getClass($col).$this->getStyle($col).$this->getId($col).'><strong>'.$col->getValue().'</strong></td>';
-						$found = true;
-						break;
-					}
-				}
-				if(!$found){
-					$html .= '<td></td>';
-				}
-			}
-			$html .= '</tr>';
-		}
+        }
+        $html .= '</tr></thead>';
+        return $html;
+    }
 
-		$html .= '</tbody>';
-		return $html;
-	}
+    protected function renderBody(){
+        $html = '<tbody>';
+        foreach($this->data['rows'] as $row){
+            $html .= '<tr>';
+            foreach($this->data['headers'] as $header){
+                $found = false;
+                foreach($row as $key => $col){
+                    if($header->getValue() == $key){
+                        $html .= '<td '.$this->getClass($col).$this->getStyle($col).$this->getId($col).'>'.$col->getValue().'</td>';
+                        $found = true;
+                        break;
+                    }
+                }
+                if(!$found){
+                    $html .= '<td></td>';
+                }
+            }
+            $html .= '</tr>';
+        }
+        if(count($this->data['sums'])){
+            $html .= '<tr>';
+            foreach($this->data['headers'] as $header){
+                $found = false;
+                foreach($this->data['sums'] as $key => $col){
+                    if($header->getValue() == $key){
+                        $html .= '<td '.$this->getClass($col).$this->getStyle($col).$this->getId($col).'><strong>'.$col->getValue().'</strong></td>';
+                        $found = true;
+                        break;
+                    }
+                }
+                if(!$found){
+                    $html .= '<td></td>';
+                }
+            }
+            $html .= '</tr>';
+        }
 
-	protected function getClass($object){
-		if($object->getClass()) return 'class="'.$object->getClass().'" ';
+        $html .= '</tbody>';
+        return $html;
+    }
 
-		return null;
-	}
+    protected function getClass($object){
+        if($object->getClass()) return 'class="'.$object->getClass().'" ';
 
-	protected function getStyle($object){
-		if($object->getStyle()) return 'style="'.$object->getStyle().'" ';
+        return null;
+    }
 
-		return null;
-	}
+    protected function getStyle($object){
+        if($object->getStyle()) return 'style="'.$object->getStyle().'" ';
 
-	protected function getId($object){
-		if($object->getId()) return 'id="'.$object->getId().'" ';
+        return null;
+    }
 
-		return null;
-	}
+    protected function getId($object){
+        if($object->getId()) return 'id="'.$object->getId().'" ';
+
+        return null;
+    }
 }
--- src/Renders/PdfRenderer.php
+++ src/Renders/PdfRenderer.php
@@ -4,17 +4,18 @@ namespace Dialect\Tablify\Renders;
 use Barryvdh\DomPDF\Facade as PDF;
 
 class PdfRenderer extends HtmlRenderer {
-	protected $documentName, $data;
-	function __construct($data, $documentName = 'tablify') {
-		$this->data = $data;
-		$this->documentName = $documentName;
-	}
+    protected $documentName, $data, $footer;
+    function __construct($data, $documentName = 'tablify', $footer = null) {
+        $this->data = $data;
+        $this->documentName = $documentName;
+        $this->footer = $footer;
+    }
 
 
-	public function render() {
-		$html = Parent::render();
-		$pdf = PDF::loadView("tablify::pdf", ["html" => $html]);
-		return $pdf->download($this->documentName);
+    public function render($footer = null) {
+        $html = Parent::render($this->footer);
+        $pdf = PDF::loadView("tablify::pdf", ["html" => $html]);
+        return $pdf->download($this->documentName);
 
-	}
+    }
 }
